ce76fd5404f418f991dbaaa0039945bb08a686d4
Oleg Sovetovs <oleg.sovetovs@outlook.com>
2026-07-22 20:18:44 -0700

    sidebar styling fixes for latest figma

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,28 +1,83 @@
 import { useDispatch, useSelector } from "react-redux";
 import {
-  BsMap,
-  BsWind,
-  BsDiagram3,
   BsCupHot,
-  BsTv,
+  BsDiagram3,
   BsDroplet,
-  BsGrid1X2,
   BsGrid,
+  BsGrid1X2,
   BsGrid3X3Gap,
+  BsMap,
+  BsTv,
+  BsWind,
+  BsX,
 } from "react-icons/bs";
-import styles from "./Sidebar.module.css";
+
 import {
+  resetFilters,
   setLocation,
-  toggleFilter,
   setVehicleType,
+  toggleFilter,
 } from "../../store/filterSlice";
+
+import styles from "./Sidebar.module.css";
+
+const EMPTY_FILTERS = {
+  location: "",
+  AC: false,
+  Automatic: false,
+  Kitchen: false,
+  TV: false,
+  Bathroom: false,
+  vehicleType: "",
+};
+
+const equipmentOptions = [
+  {
+    name: "AC",
+    icon: <BsWind aria-hidden="true" />,
+  },
+  {
+    name: "Automatic",
+    icon: <BsDiagram3 aria-hidden="true" />,
+  },
+  {
+    name: "Kitchen",
+    icon: <BsCupHot aria-hidden="true" />,
+  },
+  {
+    name: "TV",
+    icon: <BsTv aria-hidden="true" />,
+  },
+  {
+    name: "Bathroom",
+    icon: <BsDroplet aria-hidden="true" />,
+  },
+];
+
+const vehicleTypes = [
+  {
+    name: "Van",
+    value: "panel truck",
+    icon: <BsGrid1X2 aria-hidden="true" />,
+  },
+  {
+    name: "Fully Integrated",
+    value: "fully integrated",
+    icon: <BsGrid aria-hidden="true" />,
+  },
+  {
+    name: "Alcove",
+    value: "alcove",
+    icon: <BsGrid3X3Gap aria-hidden="true" />,
+  },
+];
 
 const Sidebar = ({ onSearch }) => {
   const dispatch = useDispatch();
   const filters = useSelector((state) => state.filters);
 
-  const handleLocationChange = (e) => {
-    dispatch(setLocation(e.target.value));
+  const handleLocationChange = (event) => {
+    dispatch(setLocation(event.target.value));
   };
 
   const handleEquipmentChange = (name) => {
@@ -33,101 +88,91 @@ const Sidebar = ({ onSearch }) => {
     dispatch(setVehicleType(value));
   };
 
-  const handleSearch = (e) => {
-    e.preventDefault();
+  const handleSearch = (event) => {
+    event.preventDefault();
     onSearch(filters);
   };
 
-  return (
-    <form onSubmit={handleSearch} className={styles.sidebar}>
-      <h3 className={styles.location}>Location</h3>
+  const handleClear = () => {
+    dispatch(resetFilters());
+    onSearch(EMPTY_FILTERS);
+  };
 
-      <div className={styles.inputContainer}>
-        <BsMap className={styles.icon} aria-hidden="true" />
-        <input
-          type="text"
-          value={filters.location || ""}
-          onChange={handleLocationChange}
-          placeholder="Enter location"
-          className={styles.input}
-        />
-      </div>
+  return (
+    <form className={styles.sidebar} onSubmit={handleSearch}>
+      <label className={styles.locationLabel}>
+        <span className={styles.locationTitle}>Location</span>
+
+        <span className={styles.inputContainer}>
+          <BsMap className={styles.locationIcon} aria-hidden="true" />
+
+          <input
+            type="text"
+            value={filters.location}
+            onChange={handleLocationChange}
+            placeholder="City"
+            className={styles.locationInput}
+          />
+        </span>
+      </label>
 
       <p className={styles.filterTitle}>Filters</p>
 
-      <h3 className={styles.heading}>Vehicle Equipment</h3>
-      <svg className={styles.svgLine} aria-hidden="true">
-        <use href="../../assets/divider.svg" />
-      </svg>
+      <fieldset className={styles.filterGroup}>
+        <legend className={styles.groupTitle}>Vehicle Equipment</legend>
 
-      <div className={styles.equipmentOptions}>
-        {[
-          { name: "AC", icon: <BsWind aria-hidden="true" /> },
-          { name: "Automatic", icon: <BsDiagram3 aria-hidden="true" /> },
-          { name: "Kitchen", icon: <BsCupHot aria-hidden="true" /> },
-          { name: "TV", icon: <BsTv aria-hidden="true" /> },
-          { name: "Bathroom", icon: <BsDroplet aria-hidden="true" /> },
-        ].map(({ name, icon }) => (
-          <label
-            key={name}
-            className={`${styles.label} ${filters[name] ? styles.selected : ""}`}
-          >
-            <input
-              type="checkbox"
-              checked={!!filters[name]}
-              onChange={() => handleEquipmentChange(name)}
-            />
-            <span className={styles.labelText}>
-              {icon} {name}
-            </span>
-          </label>
-        ))}
+        <div className={styles.options}>
+          {equipmentOptions.map(({ name, icon }) => (
+            <label className={styles.option} key={name}>
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                checked={filters[name]}
+                onChange={() => handleEquipmentChange(name)}
+              />
+
+              <span className={styles.optionIcon}>{icon}</span>
+              <span>{name}</span>
+            </label>
+          ))}
+        </div>
+      </fieldset>
+
+      <fieldset className={styles.filterGroup}>
+        <legend className={styles.groupTitle}>Vehicle Type</legend>
+
+        <div className={styles.options}>
+          {vehicleTypes.map(({ name, value, icon }) => (
+            <label className={styles.option} key={value}>
+              <input
+                className={styles.radio}
+                type="radio"
+                name="vehicleType"
+                checked={filters.vehicleType === value}
+                onChange={() => handleVehicleTypeChange(value)}
+              />
+
+              <span className={styles.optionIcon}>{icon}</span>
+              <span>{name}</span>
+            </label>
+          ))}
+        </div>
+      </fieldset>
+
+      <div className={styles.actions}>
+        <button type="submit" className={styles.searchButton}>
+          Search
+        </button>
+
+        <button
+          type="button"
+          className={styles.clearButton}
+          onClick={handleClear}
+        >
+          <BsX aria-hidden="true" />
+          Clear filters
+        </button>
       </div>
-
-      <h3 className={styles.heading}>Vehicle Type</h3>
-      <svg className={styles.svgLine} aria-hidden="true">
-        <use href="../../assets/divider.svg" />
-      </svg>
-
-      <div className={styles.vehicleTypes}>
-        {[
-          {
-            name: "Van",
-            icon: <BsGrid1X2 aria-hidden="true" />,
-            value: "panel truck",
-          },
-          {
-            name: "Fully Integrated",
-            icon: <BsGrid aria-hidden="true" />,
-            value: "fully integrated",
-          },
-          {
-            name: "Alcove",
-            icon: <BsGrid3X3Gap aria-hidden="true" />,
-            value: "alcove",
-          },
-        ].map(({ name, icon, value }) => (
-          <label
-            key={value}
-            className={`${styles.label} ${
-              filters.vehicleType === value ? styles.selected : ""
-            }`}
-          >
-            <input
-              type="radio"
-              checked={filters.vehicleType === value}
-              onChange={() => handleVehicleTypeChange(value)}
-            />
-            <span className={styles.labelText}>
-              {icon} {name}
-            </span>
-          </label>
-        ))}
-      </div>
-
-      <button type="submit" className={styles.searchButton}>
-        Search
-      </button>
     </form>
   );
 };
