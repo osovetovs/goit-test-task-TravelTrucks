26186a22a117492f98c732651d22a3a0ca53e77b
Oleg Sovetovs <oleg.sovetovs@outlook.com>
2026-07-22 20:12:18 -0700

    home page update to latest figma

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,17 +4,19 @@ import styles from "./Home.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.container}>
+    <main className={styles.container}>
       <div className={styles.hero}>
         <h1 className={styles.heroTitle}>Campers of your dreams</h1>
-        <p className={styles.title}>
+
+        <p className={styles.heroDescription}>
           You can find everything you want in our catalog
         </p>
-        <Link to="/catalog">
-          <button className={styles.heroButton}>View Now</button>
+
+        <Link className={styles.heroButton} to="/catalog">
+          View Now
         </Link>
       </div>
-    </div>
+    </main>
   );
 };
 
