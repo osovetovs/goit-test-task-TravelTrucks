c8267e86a488e635e6dd9385378b3b00aab6bb9f
Oleg Sovetovs <oleg.sovetovs@outlook.com>
2026-07-22 21:43:12 -0700

    align home hero with Figma

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,17 +5,19 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <main className={styles.container}>
-      <div className={styles.hero}>
-        <h1 className={styles.heroTitle}>Campers of your dreams</h1>
+      <section className={styles.hero}>
+        <div className={styles.heroText}>
+          <h1 className={styles.heroTitle}>Campers of your dreams</h1>
 
-        <p className={styles.heroDescription}>
-          You can find everything you want in our catalog
-        </p>
+          <p className={styles.heroDescription}>
+            You can find everything you want in our catalog
+          </p>
+        </div>
 
         <Link className={styles.heroButton} to="/catalog">
           View Now
         </Link>
-      </div>
+      </section>
     </main>
   );
 };
